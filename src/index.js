0d687ba95ac541de2d51edc92bb056125016ccba
Lumenatom <lumenatom@gmail.com>
2022-05-03 20:42:48 +0300

    lesson 44

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ import StoreContext, { Provider } from './StoreContext';
 
 
 let renderTree = () => {
-    
+
 
     const root = ReactDOM.createRoot(document.getElementById('root'));
     root.render(
         <React.StrictMode>
-            <Provider  store={store}>
-            <App />
+            <Provider store={store}>
+                <App />
             </Provider>
         </React.StrictMode>
     );
@@ -31,7 +31,7 @@ let renderTree = () => {
 
 renderTree(store.getState());
 
-store.subscribe( () => {
+store.subscribe(() => {
     let state = store.getState();
     renderTree(state);
 });
